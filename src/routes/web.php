3d77f601e33edfe1caf36de0679c16887b2b6065
tomomi.okawa <tomomiokawa99@gmail.com>
2024-10-04 14:18:17 +0900

    todo_modyfy

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+// コントローラの追加
+use App\Http\Controllers\Todocontroller;
+use App\Http\Controllers\CategoryController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +17,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// indexの表示
+Route::get('/', [Todocontroller::class, 'index']);
+
+// Todoの追加
+Route::post('/todos', [Todocontroller::class, 'store']);
+
+// Todoの更新
+Route::patch('todos/update', [Todocontroller::class, 'update']);
+
+// Todoの削除
+Route::delete('todos/delete', [Todocontroller::class, 'destroy']);
+
+// categoryの表示
+Route::get('/categories', [CategoryController::class, 'index']);
+
+// categoryの追加
+Route::post('/categories', [CategoryController::class, 'store']);
+
+// categoryの更新
+Route::patch('/categories/update', [CategoryController::class, 'update']);
+
+// categoryの削除
+Route::delete('/categories/delete', [CategoryController::class, 'destroy']);
+
+// Todoの検索
+Route::get('/todos/search', [TodoController::class, 'search']);
